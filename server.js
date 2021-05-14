b53ab6e09b09152d38637ec667f47fc7b8ea0355
paras <parasmanikc@gmail.com>
2021-05-14 09:52:21 +0545

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req,res) => {
-    res.send('Welcome to the home page baby')
+    res.send('Updatred')
 })
 
 const port = process.env.PORT || 3000;
